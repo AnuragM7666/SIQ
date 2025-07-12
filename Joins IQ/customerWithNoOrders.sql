a0c3ff021797af3edcac143cddad84e75e8c927f
AnuragM7666 <anuragmankapure1008@gmail.com>
2025-07-12 18:00:01 +0530

    done a join Q

---
+++ Joins IQ/customerWithNoOrders.sql
@@ -0,0 +1,21 @@
+-- Problem: Find all customers from the Customers table who have never placed any orders.
+-- The Customers table has columns: id, name.
+-- The Orders table has columns: id, customerId.
+-- Return the names of customers who do not have any corresponding entries in the Orders table.
+-- The result can be returned in any order.
+
+select c.name as Customer
+from Customers c    
+where not exists (
+    select 1 -- Not exists is TRUE only if the subquery returns no rows.
+    from Orders o 
+    where o.customerId = c.id
+);
+
+or
+
+select c.name as Customer
+from Customers c
+left join Orders o
+on c.id = o.customerId
+where o.id is null;
