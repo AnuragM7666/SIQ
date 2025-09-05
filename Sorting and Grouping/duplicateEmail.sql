b70ecbfa7dc86e8a71bf83d7f2169528c83f6ae2
AnuragM7666 <anuragmankapure1008@gmail.com>
2025-09-05 19:24:42 +0530

    optimized duplicateEmail.sql

--- Sorting and Grouping/duplicateEmail.sql
+++ Sorting and Grouping/duplicateEmail.sql
@@ -1,7 +1,16 @@
 -- Question: Write a query to find all email addresses in the Person table that occur more than once.
 -- This identifies duplicate email entries.
 
+--faster , optimal
 select email
 from Person
 group by email
 having count(email) > 1;
+
+-- cleaner due to sub query
+select email from Person 
+where email in(
+    select email from Person
+    group by email
+    having count(email) > 1
+);
